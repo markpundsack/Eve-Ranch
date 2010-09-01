9f8fd55c2dcb1284ab8d86565f60e504ccd3fe78
Mark Pundsack <mark@chiliranch.com>
2010-09-01 14:02:53 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EveRanch::Application.config.secret_token = '33071e44d06c08969212c0c669fd73099f9a7e8a8e4efa1c645585eee7cfc82a4d795c8dad41555ddf74c46278757d382d4b79f82f54d63848f68f710ee7a2e8'
